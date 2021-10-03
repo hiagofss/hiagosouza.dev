2b62169726325d378cc3f260c0ce3088f77d5e8d
Hiago Souza <hiagofss98@gmail.com>
2021-10-03 16:53:44 -0300

    Apply dynamic Header

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,8 +1,10 @@
 import About from '../components/About';
+import Header from '../components/Header';
 
 export default function Blog() {
 	return (
 		<div>
+			<Header title='Blog - Hiago Souza' />
 			<main className='max-w-4xl mx-auto mt-16 antialiased'>
 				<div className='container px-4 mx-auto'>
 					<h1>Blog</h1>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,11 @@
 import Head from 'next/head';
 import About from '../components/About';
+import Header from '../components/Header';
 
 export default function Home() {
 	return (
 		<div className='space-y-14 lg:space-y-24'>
-			<Head>
-				<title>Hiago Souza</title>
-				<link rel='icon' href='/favicon.ico' />
-			</Head>
-
+			<Header title='About - Hiago Souza' />
 			<main className='max-w-4xl mx-auto mt-16 antialiased'>
 				<About />
 			</main>
